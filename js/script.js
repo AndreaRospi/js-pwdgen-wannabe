18f5408277e6b0f95c5e0f2e9a1b401819e1fef0
Andrea <andr3z45@gmail.com>
2021-11-04 16:04:28 +0100

    aggiunta prompt stringhe concatenate e stamp

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
-const myName = prompt(nome);
+const myName = prompt('inserisci nome');
+// console.log(myName);
 
+const lastName = prompt('inserisci cognome');
+// console.log(lastName);
+
+const myColor = prompt('inserisci colore preferito');
+// console.log(myColor);
+
+const password = myName + lastName + myColor + '21'
+document.getElementById('password').innerHTML = password
+// console.log(password);
